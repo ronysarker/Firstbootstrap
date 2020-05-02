a6079062cb508fb81ad2265694cff9d46bca6095
mrony338 <md_rony@comcast.com>
2020-05-02 12:09:12 -0400

    Week 4 Assignment

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 $(function () {
     $('[data-toggle="popover"]').popover();
-})
-$(function() {
-    $(".carousel").carousel( { interval: 2000 } );
-    $("#carouselButton").click(function(){
+    $(".carousel").carousel({ interval: 2000 });
+    $("#carouselButton").click(function () {
         if ($("#carouselButton").children("i").hasClass("fa-pause")) {
             $(".carousel").carousel("pause");
             $("#carouselButton").children("i").removeClass("fa-pause");
@@ -11,7 +9,27 @@ $(function() {
         } else {
             $(".carousel").carousel("cycle");
             $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause"); 
+            $("#carouselButton").children("i").addClass("fa-pause");
         }
     });
+    $("#reserveButton").click(function () {
+        $("#reserveModal").modal("show");
+    });
+    $("#loginButton").click(function () {
+        $("#loginModal").modal("show");
+    });
+    $("#Breadcrumb").click(function () {
+        $("#imgmodal").modal("show");
+        $("#imge").attr("src", "img/breadcrumb-trail.jpg");
+    });
+    $("#lake").click(function () {
+        $("#imgmodal").modal("show");
+        $("#imge").attr("src", "img/react-lake.jpg");
+    });
+    $("#logo").click(function () {
+        $("#imgmodal").modal("show");
+        $("#imge").attr("src", "img/bootstrap-thumb.png");
+    });
 });
+  
+
